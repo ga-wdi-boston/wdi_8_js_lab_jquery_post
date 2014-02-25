c03f33c8f1a38751999ffbcebe54100442fafe92
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-25 18:47:47 -0500

    working on refactoring and css

--- js/article_list.js
+++ js/article_list.js
@@ -1,13 +1,13 @@
 var AjaxDemo = AjaxDemo || {};
 
-// Only need one Article List 
+// Only need one Article List
 AjaxDemo.ArticleList = {}
 
-// Initialize with the 
+// Initialize with the
 AjaxDemo.ArticleList.init = function(getUrl, articleListEl){
   this.url = getUrl,
   this.articleListEl = articleListEl;
-  
+
   // Set the get articles handler
   $('#get-articles').click(this.getArticles.bind(this));
 
@@ -34,13 +34,13 @@ AjaxDemo.ArticleList.createArticle = function(event){
   // Create and send a POST request
   $.ajax({
     type: "POST",
-    url: this.url + '/articles', 
+    url: this.url + '/articles',
     data: {article:  {title: $title.val(), body: $body.val()}},
     dataType: 'json',
     success: function(response){
       // Add the article to the article list
       var article = new AjaxDemo.Article(
-        response.article.id, 
+        response.article.id,
         response.article.title,
         response.article.body);
 
