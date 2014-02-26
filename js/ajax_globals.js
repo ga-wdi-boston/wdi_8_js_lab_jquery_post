c1e4509b9518473610b4f6050f36761e553a9504
tsyan <anna.tsykalova@gmail.com>
2014-02-26 07:54:43 -0500

    annotated file with comments

--- js/ajax_globals.js
+++ js/ajax_globals.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
   // Ajax Global Functions
 
-  // Fires at beginning of Ajax Request
+  // fires at beginning of any ajax request in the whole app
   $(document).ajaxStart(function(){
+
+    // the .show() function changes the css "display" property of the selected element (here, to "inline")
     $("#ajaxSpinnerImage").show();
+
+  // fires at end of any ajax request in the whole app
   }).ajaxStop(function(){
-      // Fires when Ajax Request is done
+
+      // changes the css "display" property back to its initial value (here, to "none")
       $("#ajaxSpinnerImage").hide();
-    })
-  .ajaxError(function( event, request, settings ) {
+
+  // fires if there is an error in any ajax request in the whole app
+  }).ajaxError(function( event, request, settings ) {
+
+    // appends the error to an existing div#msg but we don't have this div
     $( "#msg" ).append( "<li>Error requesting page " + settings.url + "</li>" );
   });
-  
+
 });
